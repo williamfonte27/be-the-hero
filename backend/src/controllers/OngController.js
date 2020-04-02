b76a8b6fa498868c58e7ac773b49c45f4c56fa6e
Sérgio Fonte <william_fonte@hotmail.com>
2020-04-02 13:39:53 -0300

    Add testes da aplicação

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,4 +1,4 @@
-const crypo = require('crypto');
+const generateUniqueId = require('../utils/generateUniqueId');
 const connection = require('../database/connection');
 
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
     async create(request, response) {
         const { name, email, whatsapp, city, uf } = request.body;
 
-        const id = crypo.randomBytes(4).toString('HEX');
+        const id = generateUniqueId();
 
         await connection('ongs').insert({
             id,
--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,8 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connetion = knex(configuration.development);
+const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
+
+const connetion = knex(config);
 
 module.exports = connetion;
